fda3af1cadc86763a4425c0590a33e611c3e34d1
rizwan <rizwa@LAPTOP-2QMDMD8P>
2023-01-09 18:08:06 +0530

    Fix Models Name

--- AHOY.Models/Hotel/HotelBooking.cs
+++ AHOY.Models/Hotel/HotelBooking.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 
 namespace AHOY.Models.Hotel
 {
-    public class HotelRating
+    public class HotelBooking
     {
         public long HotelId { get; set; }
-        public int HotelBookingId { get; set; }
-        public int RatingCount { get; set; }
-        public string RatingDescription { get; set; }
+        public DateTime CheckedInDate { get; set; }
+        public DateTime? CheckedOutDate { get; set; }
+        public int RoomCount { get; set; }
+        public List<Guest> GuestList { get; set; }
+        public string BookingBy { get; set; }
+        public decimal AdvanceAmount { get; set; }
     }
 }
--- AHOY.Models/Hotel/HotelRating.cs
+++ AHOY.Models/Hotel/HotelRating.cs
@@ -7,14 +7,11 @@ using System.Threading.Tasks;
 
 namespace AHOY.Models.Hotel
 {
-    public class HotelBooking
+    public class HotelRating
     {
         public long HotelId { get; set; }
-        public DateTime CheckedInDate { get; set; }
-        public DateTime? CheckedOutDate { get; set; }
-        public int RoomCount { get; set; }
-        public List<Guest> GuestList { get; set; }
-        public string BookingBy { get; set; }
-        public decimal AdvanceAmount { get; set; }
+        public int HotelBookingId { get; set; }
+        public int RatingCount { get; set; }
+        public string RatingDescription { get; set; }
     }
 }
